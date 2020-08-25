93d2527efd0d7692bc574dec99396406eeed2563
Michael Perez <mperez6473@gmail.com>
2020-08-25 11:49:25 -0500

    Most if not all sections added.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header";
+import Main from "./components/main";
+import { Projects } from "./components/projects";
+import About from "./components/about";
+import Contact from "./components/contact";
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Main />
+            <Projects />
+            <About />
+            <Contact />
         </div>
     );
 }
