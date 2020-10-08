458d1a839cf6c586721badc8278ad15decc6acea
Michael Perez <mperez6473@gmail.com>
2020-10-08 11:38:51 -0500

    Updated about me section paragraph

--- react-portfolio/src/components/about/index.jsx
+++ react-portfolio/src/components/about/index.jsx
@@ -15,16 +15,16 @@ export default function About() {
               <i class="fab fa-css3 fa-3x"></i>
             </div>
             <div>
-              <h5>React.js</h5>
-              <i class="fab fa-react fa-3x"></i>
-            </div>
-            <div>
               <h5>JavaScript</h5>
               <i class="fab fa-js-square fa-3x"></i>
             </div>
             <div>
               <h5>Python</h5>
               <i class="fab fa-python fa-3x"></i>
+            </div>
+            <div>
+              <h5>React.js</h5>
+              <i class="fab fa-react fa-3x"></i>
             </div>
             <div>
               <h5>Node.js</h5>
@@ -34,20 +34,23 @@ export default function About() {
           <div>
             <p class="aboutPadding">
               I am currently in the Tulsa, Oklahoma area. Before I was able to
-              code up a website, I was working at walgreens. I was a shift
-              leader there. After some time I wanted to reach out and get a
-              better paying job. On my freetime, I was learning how to code in
-              Python. After some time with coding, my brain was filing with
-              amazing feeling endorphins. After some time I stumbled upon a
-              bootcamp, Lambda School, from which I have been gathering most my
-              coding skills. When I have had some time to myself, I have decided
-              to look into some extra coding skills that were not requested by
-              the curriculum. Just a couple extra bits that I kept hearing would
-              be a good litle bit of coding knowledge to have. I have learned
-              how to write the basic website languages of the internet, HTML and
-              CSS. After that was nice and grasped, I was able to pickup
-              JavaScript. We are going thru React and at some point I will
-              continue on the path of Python.
+              code up a website, I was working at Walgreens as a shift lead.
+              After time, I believed I could be doing more with my time and I
+              could be getting paid more for my work. On my freetime, I was
+              learning how to code the basics in Python. Coding was amazing! As
+              a user, I would type in words and special functions and magic
+              would appear on the screen all due to the code typed in. After
+              some time I stumbled upon a bootcamp, Lambda School, from which I
+              have been taught most my coding skills. When I have had some time
+              to myself, I have decided to look into some extra coding skills
+              that were not requested by the curriculum. Just a couple extra
+              bits that I kept hearing would be a good litle bit of coding
+              knowledge to have. Through Lambda learned how to write the basic
+              website languages of the internet, HTML and CSS. After that was
+              nice and grasped, I was able to pickup JavaScript and then move up
+              to React.js and Node.js with Express. Lambda even went through
+              Computer Science fundemantals while teaching Python through their
+              curriculum.
             </p>
           </div>
         </div>
